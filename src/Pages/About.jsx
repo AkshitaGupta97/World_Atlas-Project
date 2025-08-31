48b58e5ca9af07c64490f48c11f24c45af21e2eb
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-08-31 16:31:29 +0530

    about section completed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,45 @@
+import countryData from '../Components/Layout/API/countryData.json';
+
+
+
+
+
+
 export const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <section className="section-about container">
+            <h2 className="container-title">
+                Intresting Facts of country
+                <br />
+                We're proud of.
+            </h2>
+
+            <div className="gradient-cards">
+                {
+                    countryData.countryFacts.map((country) => {
+                        const {id, countryName, capital, population, interestingFacts} = country
+                        return (
+                            <div className="card" key={id}>
+                                <div className="container-card bg-blue-box">
+                                    <p className="card-title">{countryName}</p>
+                                    <p>
+                                        <span className="card-description">Capital : </span> {capital}
+                                    </p>
+                                    <p>
+                                        <span className="card-description">Population : </span> {population}
+                                    </p>
+                                    <p>
+                                        <span className="card-description">Interesting Facts : </span> {interestingFacts}
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
+
+            </div>
+        </section>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,12 @@
-import {FaLongArrowAltRight} from 'react-icons/fa'
+import { HeroSection } from "../Components/Layout/UI/HeroSection"
+import { About } from "./About"
+
 
 export const Home = () => {
     return (
         <div>
-            <main className="hero-section main">
-                <div className="container grid grid-two-cols">
-
-                    <div className="hero-content">
-                        <h1 className="heading-xl">
-                            Explore the World, With Atlas World
-                        </h1>
-                        <p className="paragraph">
-                            🌍 AtlasWorld... Atlases today include not just maps, but data on population, climate, biodiversity,
-                             and even space exploration. The legendary city of Atlantis was said to be ruled by a king named Atlas, 
-                             and its name means “Island of Atlas”
-                        </p>
-                        <button className="btn btn-darken btn-inline bg-white-box">
-                            Sart Exploring <FaLongArrowAltRight />
-                        </button>
-                    </div>
-                    <div className="hero-image">
-                        <img src="https://png.pngtree.com/png-vector/20231223/ourmid/pngtree-earth-globe-isolated-on-white-background-map-png-image_11059520.png" alt="" />
-                    </div>
-                </div>
-            </main>
-
+            <HeroSection />
+            <About />
         </div>
     )
 }
